bc03f93f5ba2ad564d1706043adb79b91f880c52
pyoungkangkim <pyoungkangkim@users.noreply.github.com>
2016-09-14 16:46:20 -0400

    Documented pre and post conditions and member contributions

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,26 +1,31 @@
 /*
- * pre:      puzzle != null, guesses != null, limit >=0
- * post:     returns the number of the guess that solves the 
- *           puzzle within the given limit on incorrect guesses.  
- *           If the puzzle is not solved within the limit, returns -1.
- *           e.g. 
- *              hangman ("cat", "abct", 1) is 4  (solved)
- *              hangman ("cat", "abct", 0) is -1 (hit the limit)
- *              hangman ("cat", "abcd", 5) is -1 (gave up)
- *              
- *    For this assignment, Alex Duan, Pyoung Kang Kim and Daniel Quintero collaborated.
- *    Alex wrote the code for the hangman method, Pyoung wrote the JUnit test suite,
- *    and Daniel helped to debug the hangman method and checked code coverage.
-*/
+ * pre:	     
+ * post:    If puzzle == null or guesses == null throw NullPointerException
+ *          else if limit >= 0 throw IllegalArgumentException
+ * 	    else returns the number of the guess that solves the 
+ *          puzzle within the given limit on incorrect guesses.  
+ *          If the puzzle is not solved within the limit, returns -1.
+ *          e.g. 
+ *             hangman ("cat", "abct", 1) is 4  (solved)
+ *             hangman ("cat", "abct", 0) is -1 (hit the limit)
+ *             hangman ("cat", "abcd", 5) is -1 (gave up) 
+ *
+ * For this assignment, 
+ * Alex Duan wrote state checkers in the hangman method for if puzzle == null and if guesses == null.
+ * Daniel Quintero wrote a state checker for if !(limit >= 0) and wrote two JUnit tests for NullPointerException 
+ *  	and two for IllegalArgumentException
+ * Pyoung Kang Kim documented the changes in the preconditions and postconditions and cleaned up some of the code 
+ * 	formatting and wrote what each member did.
+ * /
 public class Hangman {
 	
 	static public int hangman (String puzzle, String guesses, int limit) 
 	{
-		if(puzzle==null)
+		if(puzzle == null)
 			throw new NullPointerException("Puzzle Cannot be null");
-		if(guesses==null)
+		if(guesses == null)
 			throw new NullPointerException("Guesses Cannot be null");
-		if(!(limit >=0))
+		if(!(limit >= 0))
 			throw new IllegalArgumentException("Limit must be >= 0");
 
 		char g[]=guesses.toCharArray();
