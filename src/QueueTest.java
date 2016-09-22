4e30aeddbce532d57c5ec817e0ac80e848023252
pyoungkangkim <pyoungkangkim@users.noreply.github.com>
2016-09-22 09:52:31 -0400

    Final QueueTest edit before release.

--- src/QueueTest.java
+++ src/QueueTest.java
@@ -1,44 +1,71 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class QueueTest {
-	Queue q;
-	
+	Queue<String> q;
+
 	@Before
-	public void initQueue(){
-		q= new Queue();
+	public void initQueue() {
+		q = new Queue<String>();
 	}
-	
+
+	@Test(expected = IllegalStateException.class)
+	public void peekOnEmptyQueueThrowsIllegalStateException() {
+		q.peek();
+	}
+
 	@Test
-	public void testenQueueAndPeek(){
+	public void testenQueueAndPeekOneElement() {
 		q = q.enQueue(new String("hello"));
 		assertEquals("hello", q.peek());
 	}
-	
-	@Test (expected = IllegalArgumentException.class)
-	public void testenQueueNullObject(){
+
+	@Test
+	public void testEnQueueAndPeekTwoElements() {
+		q = q.enQueue(new String("cat"));
+		q = q.enQueue(new String("dog"));
+		assertEquals("cat", q.peek());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testenQueueNullObject() {
 		q = q.enQueue(null);
 	}
-	
+
+	@Test(expected = IllegalArgumentException.class)
+	public void enQueueNullValuesThrowsIllegalArgumentException() {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add(null);
+		q = new Queue<String>(list);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void deQueueOnEmptyQueueThrowsIllegalStateException() {
+		q.deQueue();
+	}
+
 	@Test
-	public void testDeQueue(){
+	public void testDeQueue() {
 		q = q.enQueue(new String("hello"));
 		q = q.enQueue(new String("hi"));
 		q = q.deQueue();
 		assertEquals("hi", q.peek());
 	}
-	
+
 	@Test
-	public void testisEmpty(){
-		assertTrue(q.isEmpty());
-	}
-	
-	@Test
-	public void testisEmptyAfterDequeue(){
+	public void testisEmptyAfterDequeue() {
 		q = q.enQueue(new String("hello"));
 		q = q.deQueue();
+		assertTrue(q.isEmpty());
+	}
+
+	@Test
+	public void testisEmpty() {
 		assertTrue(q.isEmpty());
 	}
 }
