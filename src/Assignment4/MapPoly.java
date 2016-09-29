bfbbc4dc40cdf56c3a8c55ece7fcb4a3f3f92159
Pyoung Kang Kim <pkim23@masonlive.gmu.edu>
2016-09-29 09:50:13 -0400

    Added some tests for MapPoly

---
+++ src/Assignment4/MapPoly.java
@@ -0,0 +1,111 @@
+/*
+ * MapPoly:  Liskov's Poly class, converted to a Map rep.
+ * Notes:
+ *   0) Engineered for Homework 4, 2016; hence not a model implementation
+ */
+import java.util.*;
+
+public class MapPoly {
+
+    // Polys are immutable polynomials with integer coefficients
+    // A typical Poly is c0 + c1x + c2x^2 + ...
+    // NOTE For Homework 4, 2016: There *is* an expected order to the terms of a polynomial.
+
+    private Map<Integer, Integer> trms;   // WAS TreeMap IN THE IN-CLASS EXERCISES
+    
+    // Rep invariant 
+    //	TreeMap assumption, i.e map entries are stored in increasing order of exponents
+    //	For every Entry, the Value is >= 0. In an Abstract view, the exponent is >= 0.
+    
+    // Effects: Initializes this to be the zero polynomial
+    public MapPoly() {
+       this(0,0);
+    }
+
+    
+    // Effects: If n < 0 throws IllegalArgumentException
+    // else initializes this to be the polynomial c*x^n
+    public MapPoly(int c, int n) throws IllegalArgumentException {
+       if (n < 0) {
+          throw new IllegalArgumentException("MapPoly(int, int) constructor");
+       }
+       trms = new TreeMap<Integer, Integer> ();
+       if (c != 0) { trms.put(n, c); }
+    }
+
+    // Effects: returns the degree of this
+    public int degree() {
+    	   int degree  = 0;
+    	   if (trms.size() > 0) {
+    	     for (Map.Entry<Integer, Integer> entry : trms.entrySet())
+    	    	 degree = entry.getKey();        	 
+    	   }
+    	   return degree;
+    	}
+
+    // Effects: returns the coefficent of the term of this whose exponent is d
+    public int coeff(int d) {
+       if (d < 0) throw new IllegalArgumentException("MapPoly.coeff");
+       if (trms.containsKey(d)) return trms.get(d);
+       return 0;
+    }
+
+    // Effects: If q is null throw NullPointerException
+    // else return the MapPoly this - q
+    public MapPoly sub(MapPoly q) {
+       return add(q.minus());
+    }
+
+    // Effects: return the MapPoly -this
+    public MapPoly minus() {
+       MapPoly result = new MapPoly();
+       for (Integer i : trms.keySet()) {
+          result.trms.put (i, -trms.get(i));
+       }
+       return result;
+    }
+
+    // Effects: If q is null throw NullPointerException
+    // else return the MapPoly this + q
+    public MapPoly add(MapPoly q) {
+       
+       // find all the nonzero coefficients in either this or q
+       Set<Integer> nonZero = new HashSet<Integer>(q.trms.keySet());
+       nonZero.addAll(trms.keySet());
+
+       // Add the coefficients together; store the nonzero results
+       MapPoly result = new MapPoly();
+       for (Integer i : nonZero) {
+          int newCoeff = coeff(i) + q.coeff(i);
+          if (newCoeff != 0) result.trms.put(i, newCoeff);
+       }
+       return result;
+    }
+
+    // Effects: If q is null throw NullPointerException
+    // else return the MapPoly this * q
+    public MapPoly mul(MapPoly q) {
+       MapPoly result = new MapPoly();
+
+       for (Integer i:  trms.keySet()) {
+          for (Integer j:  q.trms.keySet()) {
+              result = result.add(new MapPoly (coeff(i) * q.coeff(j), i*j));
+          }
+       }
+       return result;
+    }
+
+    public String toString() {   
+       String r = "MapPoly:";
+
+       if (trms.size() == 0) {   // special case for empty Map
+           r += " " + 0;
+       }
+
+       for (Integer i: trms.keySet()) {
+          if (coeff(i) < 0) { r += " - " + -coeff(i) + "x^" + i; }
+          else              { r += " + " +  coeff(i) + "x^" + i; }
+       }
+       return r;
+    }
+}
---
+++ src/Assignment4/MapPolyTests.java
@@ -0,0 +1,63 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MapPolyTests {
+
+	MapPoly poly;
+
+	public boolean repOk(MapPoly poly) {
+		Pattern pattern = Pattern.compile("[0-9]+");
+		Matcher matcher = pattern.matcher(poly.toString());
+		int i = 0;
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		while (matcher.find()) {
+			if (i % 2 != 0)
+				list.add(Integer.parseInt(matcher.group()));
+			i++;
+		}
+		Integer[] array = new Integer[list.size()];
+		array = list.toArray(array);
+		for (int j = 0; j < array.length - 1; j++) {
+			if (array[j] > array[j + 1])
+				return false;
+		}
+		return true;
+	}
+
+	@Before
+	public void createThreeTermsPoly() {
+		poly = new MapPoly(23423434, 234234);
+		poly = poly.add(new MapPoly(999, 11));
+	}
+	
+	@Test
+	public void degreeReliesOnOrder() {
+		assertEquals(false, poly.degree() == 234234);
+	}
+
+	@Test
+	public void addBreaksMonotonicOrder() {
+		poly = new MapPoly(23423434, 234234);
+		poly = poly.add(new MapPoly(999, 11));
+		assertEquals(false, repOk(poly));
+	}
+	
+	@Test
+	public void minusBreaksOrKeepsMonotonicOrder(){
+	// ran out of time :(
+	// if minus breaks order, please prove to me with a test case
+	// if minus does not, also prove it to me 
+	}
+	
+	
+	@Test
+	public void multiplyBreaksOrKeepsMonotonicOrder(){
+	// also ran out of time. One of you guys should do each of this test cases!
+	}
+}
