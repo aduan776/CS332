2c880bed4e4630f0b21fa11b33f330444153766f
pyoungkangkim <pyoungkangkim@users.noreply.github.com>
2016-10-06 06:17:11 -0400

    Update Members2.java

--- src/Assignment5/Members2.java
+++ src/Assignment5/Members2.java
@@ -1,17 +1,19 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public class Members1 {
-	// Members1 is a mutable record of organization membership
+public class Members2 {
+	// Members2 is a mutable record of organization membership
 	/* AF:	The representation is members where it is [] if no members
 	 * 		[member] if not empty and a member joins
 	 * 		[member, anotherMember] if another member joins and so forth
 	 */
-	// rep-inv: members != null
+	// rep-inv: members != null && no duplicates in members
 
 	List<String> members; // the representation
-
-	public Members1(){
+	
+	public Members2(){
 		members = new ArrayList<String>();
 	}
 	
@@ -20,10 +22,12 @@ public class Members1 {
 	}
 	
 	public boolean repOk2(){
-		// According to the slides, this repaired version has only one rep-inv
-		return false;
+		Set<String> set = new HashSet<String>();
+		for (String member : members)
+			set.add(member);
+		return set.size() == members.size();
 	}
-	
+
 	// Post: person becomes a member
 	public void join(String person) {
 		if (!isMember(person))
@@ -37,7 +41,7 @@ public class Members1 {
 	
 	public boolean isMember(String person) {
 		if (person == null)
-			throw new NullPointerException("Members1.isMember");
+			throw new NullPointerException("Members2.isMember");
 		return members.contains(person);
 	}
 }
