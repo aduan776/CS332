b0e659d85c393cb6ea9eae1310ff307863d88431
pyoungkangkim <pyoungkangkim@users.noreply.github.com>
2016-10-13 08:08:30 -0400

    Statement Of Contributions.

--- src/Assignment6/FilterIterator.java
+++ src/Assignment6/FilterIterator.java
@@ -1,6 +1,18 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
+/* Contributions:
+ * Alex Duan wrote the crux of the FilterIterator class. He was the first to figure out what the requirements were actually truly asking for
+ * and started to code. He also helped discuss the assignment and make sure everyone understood clearly of what was to be completed.
+ *
+ * Pyoung Kang Kim helped pair program by suggesting ideas on how to implement the code and making sure requirements were met such
+ * as generics, correct method implementation and just gave another eye. He also wrote majority of the JUnit Test cases.
+ *
+ * Daniel Quintero wrote the Check Class and also helped discuss the assignment. He also helped review the code to make sure things were 
+ * implemented correctly. Afterwards he wrote a couple JUnit Test cases as well.
+ */
+
+
 public class FilterIterator<E> implements Iterator<E> {
 	Iterator<E> iter;
 
