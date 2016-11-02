e41fbb9c95a69abdfd5a8d5d90c4622ada2750e9
aduan776 <alexduan007@gmail.com>
2016-11-02 09:12:15 -0400

    initial commit of Assignment 9

---
+++ src/Point3.java
@@ -0,0 +1,28 @@
+
+public class Point3 extends Point2{
+	
+	private int z;
+	
+	public Point3(int x, int y, int z)
+	{
+		super(x, y);
+		this.z = z;
+	}
+	
+
+		public boolean equals(Object p) { //overridingdeﬁnition
+			if(p instanceof Point3)
+				return equals((Point3)p);
+			return super.equals(p); }
+		
+			public boolean equals(Point2 p) { //overridingdeﬁnition
+			if(p instanceof Point3)
+				return equals((Point3)p);
+			return super.equals(p); 
+			}
+			
+			public boolean equals(Point3 p) { //extradeﬁnition
+			if(p==null || z !=p.z)return false;
+			return super.equals(p); }
+
+}
